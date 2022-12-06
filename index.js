5e6c3d5e6a3d5ce34312ba8d7c1915b57c6f141d
LayneNi <laynenigro@gmail.com>
2022-12-06 18:42:26 -0500

    polished questions

--- index.js
+++ index.js
@@ -3,18 +3,36 @@ const inquirer = require('inquirer');
 const generateHTML = require('./utils/generateHTML')
 const path = require("path");
 const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+
 const myTeamArray = [];
 
-// const engineerQuestions;
-
-// const internQuestions;
+// const menuQuestions = () => {
+//   inquirer
+//   .prompt([
+//     {
+//       type: 'confirm',
+//       message: "Would you like to add any employees to your team?",
+//       name: 'confirmation',
+//     },
+//   ])
+//   .then((response) =>{
+//     if (confirmation === false) {
+//       console.log(myTeamArray);
+//       return console.log( "You are finished" );
+//     } else {
+//       managerQuestions()
+//     }
+//   });
+// }
 
 const managerQuestions = () => {
   inquirer
   .prompt([
     {
       type: 'input',
-      message: "What is the manager's name?",
+      message: "What is the Manager's name?",
       name: 'name',
     },
     {
@@ -40,21 +58,23 @@ const managerQuestions = () => {
     },
   ])
 .then((response) =>{
-  Manager();
-const manager = new Manager(response.office);
+const { name, id, email, office } = response
+const manager = new Manager(name, id, email, office);
+myTeamArray.push(manager);
 createEmployee(response.employee)
 })
 };
 
 const createEmployee = (employeeType) => {
   if (employeeType === "There are no more employees") {
+    console.log(myTeamArray);
     return console.log( "You are finished" );
   } else if (employeeType === "Engineer"){
     inquirer
     .prompt([
       {
         type: 'input',
-        message: "What is the manager's name?",
+        message: "What is the Engineer's name?",
         name: 'name',
       },
       {
@@ -69,8 +89,8 @@ const createEmployee = (employeeType) => {
       },
       {
         type: 'input',
-        message: "What is their office number?",
-        name: 'office',
+        message: "What is their github username?",
+        name: 'username',
       },
       {
         type: 'list',
@@ -79,13 +99,52 @@ const createEmployee = (employeeType) => {
         name: 'employee',
       },
     ])
-  //   .then((response) =>{
-  //     Engineer();
-  //   const manager = new Manager(response.office);
-  //   createEmployee(response.employee)
-  // })
+    .then((response) =>{
+      const { name, id, email, username } = response
+      const engineer = new Engineer(name, id, email, username);
+      myTeamArray.push(engineer);
+      createEmployee(response.employee)
+  })
+}else if (employeeType === "Intern"){
+  inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: "What is the Intern's name?",
+      name: 'name',
+    },
+    {
+      type: 'input',
+      message: "What is their employee ID?",
+      name: 'id',
+    },
+    {
+      type: 'input',
+      message: "What is their email address?",
+      name: 'email',
+    },
+    {
+      type: 'input',
+      message: "What school does the Intern attend?",
+      name: 'school',
+    },
+    {
+      type: 'list',
+      message: "What type of employee would you like to add next?",
+      choices: ["Engineer", "Intern", "There are no more employees"],
+      name: 'employee',
+    },
+  ])
+  .then((response) =>{
+    const { name, id, email, school } = response
+    const intern = new Intern(name, id, email, school);
+    myTeamArray.push(intern);
+    createEmployee(response.employee)
+})
 }
 }
+
+managerQuestions()
 
 
 // .then((response) => {
